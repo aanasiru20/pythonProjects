bbfc8d63390d8efe5e7021495b88ca1bcfde9a0d
Abu Abdul Nasirudeen <nasiruabuabdul@gmail.com>
2025-10-15 19:22:24 +0100

    online tutorial project

---
+++ grading.py
@@ -0,0 +1,11 @@
+print("Simple Grading Program")
+user = int(input("Enter a number: "))
+
+if user >= 90:
+	print("A")
+elif user >= 80:
+	print("A")
+elif user >= 70:
+	print("A")
+else:
+	print("Failed")
